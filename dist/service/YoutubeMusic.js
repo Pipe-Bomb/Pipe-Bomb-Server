53590f33cb3d70a96f2f6cd1c35c7c54b7999033
actions-user <action@github.com>
2023-04-21 07:06:49 +0000

    build: d78d18fb464fbf3b3620edfc6faf38158f41ad74

--- dist/service/YoutubeMusic.js
+++ dist/service/YoutubeMusic.js
@@ -1,9 +1,25 @@
 import * as YTM from "node-youtube-music";
-import YTDL from "ytdl-core";
-import Exception from "../response/Exception.js";
+import YTA from "youtube-music-api";
 import Track from "../music/Track.js";
 import StreamingService from "./StreamingService.js";
 import ServiceManager from "./ServiceManager.js";
+import Exception from "../response/Exception.js";
+import { wait } from "../Utils.js";
+const Yta = new YTA();
+let initialized = false;
+Yta.initalize().then(() => {
+    initialized = true;
+});
+export async function waitForInitialization() {
+    return new Promise(async (resolve) => {
+        if (initialized)
+            return resolve();
+        while (!initialized) {
+            await wait(100);
+        }
+        resolve();
+    });
+}
 export default class YoutubeMusic extends StreamingService {
     constructor() {
         super("Youtube Music", "ym");
@@ -34,22 +50,27 @@ export default class YoutubeMusic extends StreamingService {
         return ServiceManager.getInstance().getService("Youtube").getAudio(trackID);
     }
     async getTrack(trackID) {
+        await waitForInitialization();
         trackID = this.convertTrackIDToLocal(trackID);
-        const url = "https://www.youtube.com/watch?v=" + trackID;
         try {
-            const data = await YTDL.getInfo(url);
-            let thumbnail = data.thumbnail_url || null;
-            if (!thumbnail && data.videoDetails.thumbnails.length) {
-                thumbnail = data.videoDetails.thumbnails[0].url;
+            const data = await Yta.getSong(trackID);
+            let thumbnailSize = 0;
+            let thumbnail = null;
+            for (let thumbnailData of data.thumbnails) {
+                const newSize = thumbnailData.width * thumbnailData.height;
+                if (newSize > thumbnailSize) {
+                    thumbnailSize = newSize;
+                    thumbnail = thumbnailData.url;
+                }
             }
             return new Track(`ym-${trackID}`, {
-                title: data.videoDetails.title,
-                artists: [data.videoDetails.author.name],
+                title: data.name,
+                artists: [data.artist],
                 image: thumbnail
             });
         }
         catch (e) {
-            console.log("YTDL ERROR", url);
+            console.log("YTA ERROR", e);
             throw new Exception(e);
         }
     }
