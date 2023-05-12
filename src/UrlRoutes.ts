2a510d466c23d5de91f9bc354c3a8943c8486750
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-05-13 10:53:08 +1200

    remove unnecessary opengraph tags

--- src/UrlRoutes.ts
+++ src/UrlRoutes.ts
@@ -90,14 +90,14 @@ export default class UrlRoutes {
 <meta property="og:url" content="${url}">
 <meta property="og:type" content="website">
 <meta property="og:title" content="${title}">
-<meta property="og:description" content="${description}">
-<meta property="og:image" content="${image}">
+${description ? `<meta property="og:description" content="${description}">` : ""}
+${image ? `<meta property="og:image" content="${image}">` : ""}
 <meta name="twitter:card" content="summary_large_image">
 <meta property="twitter:domain" content="${requestInfo.address}">
 <meta property="twitter:url" content="${url}">
 <meta name="twitter:title" content="${title}">
-<meta name="twitter:description" content="${description}">
-<meta name="twitter:image" content="${image}">
+${description ? `<meta name="twitter:description" content="${description}">` : ""}
+${image ? `<meta name="twitter:image" content="${image}">` : ""}
 
 <meta http-equiv="refresh" content="2;url=${url}" />
 <script>
