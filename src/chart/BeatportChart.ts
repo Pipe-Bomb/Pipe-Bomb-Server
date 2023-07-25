5151db9a8f7654c157d4b9494650f2a2bce76db8
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-07-25 22:11:09 +1200

    fix beatport charts

--- src/chart/BeatportChart.ts
+++ src/chart/BeatportChart.ts
@@ -3,6 +3,7 @@ import Axios from "axios";
 import { convertArrayToString } from "../Utils.js";
 import ServiceManager from "../service/ServiceManager.js";
 import Track from "../music/Track.js";
+import { JSDOM } from "jsdom";
 
 export default class BeatportChart extends Chart {
     private beatportSlug: string;
@@ -16,10 +17,15 @@ export default class BeatportChart extends Chart {
         return new Promise<Track[]>(async (resolve, reject) => {
             try {
                 const { data } = await Axios.get("https://www.beatport.com/" + this.beatportSlug);
-                const raw = data.split("window.Playables = ", 2)[1].split("};\n")[0] + "}";
+                const dom = new JSDOM(data);
+                const raw = dom.window.document.querySelector("script#__NEXT_DATA__")?.textContent;
+                if (!raw) {
+                    console.error("Couldn't find script, BeatPort website structure may have changed");
+                    throw "Couldn't find script, BeatPort website structure may have changed";
+                }
                 const json = JSON.parse(raw);
                 const trackStrings: string[] = [];
-                for (let trackData of json.tracks) {
+                for (let trackData of json.props.pageProps.dehydratedState.queries[0].state.data.results) {
                     const titleString: string = trackData.name;
                     const artistString = convertArrayToString(trackData.artists.map(artist => artist.name));
                     let remixString = "";
@@ -37,6 +43,7 @@ export default class BeatportChart extends Chart {
                 this.trackList = newTracklist;
                 resolve(Array.from(newTracklist));
             } catch (e) {
+                console.error(e);
                 reject(e);
             }
         });
